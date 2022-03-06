c445c4a15d0daaff8ca873ec766e8fe9cf43e273
mlynn0791 <99066117+mlynn0791@users.noreply.github.com>
2022-03-06 11:57:41 -0500

    React Bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import {Container} from 'react-bootstrap'
+
 //arrow functions
 const App = () => {
   return (
     <>
+    <Header />
+    <main>
+      <Container>
     <h1> Welcome to My Shop!</h1>
+    </Container>
+    </main>
+    <Footer/>
     </>
   );
 }
