928cf37edd36eb6691b817129d1dd15d8af26c7f
mlynn0791 <99066117+mlynn0791@users.noreply.github.com>
2022-03-14 19:16:44 -0400

    Front End

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,30 @@
-import Footer from "./Components/Footer";
-import Header from "./Components/Header";
-import {Container} from 'react-bootstrap'
+import {Container} from 'react-bootstrap';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
+
 
 //arrow functions
 const App = () => {
   return (
     <>
-    <Header />
-    <main>
-      <Container>
-    <h1> Welcome to My Shop!</h1>
-    </Container>
-    </main>
-    <Footer/>
+      <Router>
+        <Header />
+        <main className='py-3'>
+          <Container>
+            <Routes>
+              <Route path='/' element={<HomeScreen />} exact />
+              <Route path='/product/:id' element={<ProductScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </Router>
     </>
   );
 }
-
 
 export default App;
